73afafac2432bc3ea50322fd960164d3612027ce
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-12-22 16:43:17 +0100

    Refactor the main code

--- sauto/spiders/sauto_spider.py
+++ sauto/spiders/sauto_spider.py
@@ -26,22 +26,71 @@ def log_url(func):
     return wrapper
 
 class SautoSpider(scrapy.Spider):
+    """
+    A scrapy spider used to scrape data from the website "https://www.sauto.cz".
+    It reads parameters from a JSON file and sends HTTP GET requests to the API endpoint with different price ranges.
+    The responses are parsed as JSON and the results are yielded.
+    """
+
     name = "sauto"
     BASE_URL = "https://www.sauto.cz/api/v1/items/search?"
 
+    @staticmethod
+    def read_params_from_json(file_path: str) -> dict:
+        """
+        Read parameters from a JSON file.
+
+        Args:
+            file_path (str): The path to the JSON file.
+
+        Returns:
+            dict: The parameters read from the JSON file.
+        """
+        with open(file_path, "r") as file:
+            params = json.load(file)
+        return params
+
     @log_url
     def start_requests(self):
-        with open("params.json", "r") as file:
-            params = json.load(file)
+        """
+        Read parameters from a JSON file, generate URLs with different price ranges,
+        send HTTP GET requests, and yield the responses.
+        """
+        params = self.read_params_from_json("params.json")
+        urls = self.generate_urls(params)
 
-        for price in range(0, 2000001, 200000):
-            params["price_from"] = price
-            params["price_to"] = price + 200000 if price <= 1800000 else 0    
-            url = f"{self.BASE_URL}{urlencode(params)}"
+        for url in urls:
             yield scrapy.Request(url=url, method='GET', callback=self.parse)
             self.logger.info(f"Scraping url: {url}")
 
+    def generate_urls(self, params: dict) -> list:
+        """
+        Generate URLs with different price ranges.
+
+        Args:
+            params (dict): The parameters for the API request.
+
+        Returns:
+            list: The generated URLs.
+        """
+        urls = []
+        for price in range(0, 2000001, 200000):
+            params["price_from"] = price
+            params["price_to"] = price + 200000 if price <= 1800000 else 0
+            url = f"{self.BASE_URL}{urlencode(params)}"
+            urls.append(url)
+        return urls
+
     def parse(self, response):
+        """
+        Parse the JSON response and yield the results.
+
+        Args:
+            response (scrapy.http.Response): The HTTP response.
+
+        Yields:
+            dict: The parsed results.
+        """
         try:
             data = json.loads(response.body)
             for item in data.get("results", []):
